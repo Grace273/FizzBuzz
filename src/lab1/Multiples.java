f79c9e52b773624ad458d4ddb042db2cf6a7654c
Grace Wang <84159791+Grace273@users.noreply.github.com>
2025-09-09 12:53:25 -0400

    update

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,10 +2,28 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        for(int j=1;j<1000;j++){
-            if(j/3 == 0 || j/5 == 0) {
-                System.out.println(j);
+        System.out.println(multiples());
+    }
+
+    static int multiples(int n, int a, int b) {
+        int count = 0;
+        for(int j=1;j<n;j++){
+            if(j%a == 0 || j%b == 0) {
+                count++;
             }
         }
+
+        return count;
+    }
+
+    static int multiples() {
+        int count = 0;
+        for(int j=1;j<1000;j++){
+            if(j%3 == 0 || j%5 == 0) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
